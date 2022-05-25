242a1fbbbb3f281070137d1c9f27eaf1b6901f0a
sophonTBP <lucas.jumelin@live.fr>
2022-05-25 15:05:09 +0200

    almost finished

---
+++ pages/product/success.js
@@ -0,0 +1,18 @@
+import React,{useState,useEffect} from 'react'
+import Link from 'next/link'
+import { BsBagCheckFill} from "react-icons/bs"
+import {useRouter} from "next/router"
+
+import { useStateContext } from '../../context/StateContext'
+useState
+const Success = () => {
+const {setCartItems, setTotalPrice, setTotalQuantities}
+= useStateContext();
+
+
+  return (
+    <div>success</div>
+  )
+}
+
+export default Success
